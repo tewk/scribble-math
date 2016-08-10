8832010717682b200b95f36a9d413b34c416edfe
Georges Dupéron <georges.duperon@gmail.com>
2016-08-10 23:59:59 +0200

    Fixed missing dependency

--- info.rkt
+++ info.rkt
@@ -1,8 +1,12 @@
 #lang info
 (define collection "scribble-math")
 (define deps '("base"
-               "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
+               "rackunit-lib"
+               "scribble-lib"))
+(define build-deps '("scribble-lib"
+                     "racket-doc"
+                     "at-exp-lib"
+                     "scribble-doc"))
 (define scribblings '(("scribblings/scribble-math.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
