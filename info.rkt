d9507573e7bdb0764ce0cba19e58291daf0c9a20
Georges Dupéron <georges.duperon@gmail.com>
2016-09-15 23:34:28 +0200

    Exclude MathJax/ and katex/ from raco setup and raco test.

--- info.rkt
+++ info.rkt
@@ -7,6 +7,8 @@
                      "racket-doc"
                      "at-exp-lib"
                      "scribble-doc"))
+(define compile-omit-paths '("MathJax" "katex"))
+(define test-omit-paths '("MathJax" "katex"))
 (define scribblings '(("scribblings/scribble-math.scrbl" ())))
 (define pkg-desc "Typesetting math and Asymptote figures in Scribble documents")
 (define version "0.9")
